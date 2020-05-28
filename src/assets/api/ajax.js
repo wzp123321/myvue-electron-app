34298434a9b32861924c9bd84bbe38262bcdb733
zpwan <zpwan@iflytek.com>
2020-05-28 19:21:28 +0800

    修改接口地址

--- src/assets/api/ajax.js
+++ src/assets/api/ajax.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 // 'https://music.163.com'
-const BASE_URL = 'http://127.0.0.1:3001'
+const BASE_URL = 'http://node.zpwan-yz.com'
 // 定义一个空的数组，用于存放请求中的参数
 import {
   message
